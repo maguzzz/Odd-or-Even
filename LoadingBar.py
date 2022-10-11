3233e17af4bc63cd1b222481cf3553f917cc963b
maguzzz <markusm.touray@gmail.com>
2022-10-11 20:11:47 +0200

    User input

--- LoadingBar.py
+++ LoadingBar.py
@@ -1,2 +1,11 @@
-print("-------------------------\n")
+from tokenize import Number
+
+
+print("-------------------------")
+print("What number range do you want?")
+number = input()
 print("Odd | Even\n")
+user = input()
+
+
+        
